cdffbf620a1769ff976b0f6e145e90633cae23e5
Brian Jenkins <brian.jenkins0413@gmail.com>
2020-10-20 17:07:31 -0400

    Added HTTPCLIENT jar files

--- src/DbInterface.java
+++ src/DbInterface.java
@@ -4,6 +4,7 @@ public class DbInterface {
 	//Going to implement a REST API to access IGDB video game database 
 	//https://api-docs.igdb.com/#about
 	//Apache Restful 
+	//tutorialspoint.com/apache_httpclient/apache_httpclient_environment_setup.htm
 	
 
 }
