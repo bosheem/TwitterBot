f0d6e32457fa051c4658b1af0df4ad2ec0ef2320
Brian Jenkins <brian.jenkins0413@gmail.com>
2020-10-22 21:01:09 -0400

    attempt to use apache HTTPCLIENT; issues with import FIXME

--- src/DbInterface.java
+++ src/DbInterface.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 public class DbInterface {
 	
@@ -6,5 +11,23 @@ public class DbInterface {
 	//Apache Restful 
 	//tutorialspoint.com/apache_httpclient/apache_httpclient_environment_setup.htm
 	
+    //Creating a HttpClient object
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    //Creating a HttpGet object
+    HttpGet httpget = new HttpGet("https://www.tutorialspoint.com/ ");
+
+    //Printing the method used
+    System.out.println("Request Type: "+httpget.getMethod());
+
+    //Executing the Get request
+    HttpResponse httpresponse = httpclient.execute(httpget);
+
+    Scanner sc = new Scanner(httpresponse.getEntity().getContent());
+
+    //Printing the status line
+    System.out.println(httpresponse.getStatusLine());
+    while(sc.hasNext()) {
+       System.out.println(sc.nextLine());
 
 }
